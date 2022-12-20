6e0232b2ca515cc2b875bfcb3546f2f181478249
sefaalanur <sefaalanr99@gmail.com>
2022-12-20 03:26:53 +0100

    Add project files.

---
+++ PersonList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+namespace PersonList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testConnection();
+            Console.ReadKey();
+        }
+        private static void testConnection()
+        {
+            using (NpgsqlConnection conn = GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    if (conn.State == System.Data.ConnectionState.Open)
+                    {
+                        Console.WriteLine("Connected");
+                    }
+
+                }
+                catch (NpgsqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        private static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(@"Server=localhost;Port=5434;User Id=postgres;Password=sefa2024;Database=PhoneContact");
+        }
+    }
+}
